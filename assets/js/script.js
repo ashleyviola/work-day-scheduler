2bcfd69e4da6191ae65ac3db8da11189fca92723
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-11-07 17:18:55 -0500

    save functionality fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,32 @@
+$(function () {});
 // show todays date 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-// work hours 
-let workHours = [
-    {time: "9 AM", event: ""},
-    {time: "10 AM", event: ""},
-    {time: "11 AM", event: ""},
-    {time: "12 PM", event: ""},
-    {time: "1 PM", event: ""},
-    {time: "2 PM", event: ""},
-    {time: "3 PM", event: ""},
-    {time: "4 PM", event: ""},
-    {time: "5 PM", event: ""}
-];
+let tasks = {};
 
-// create work hour rows 
-workHours.forEach(function(timeBlock, index){
-    var timeLabel = timeBlock.time;
-    // <div class = timebloack> 
-    // <div class = row>
-    // <div class = col 1> time </div>
-    // <textarea></>
-    // <div class = col11> save button 
-    var row = 
-        '<div class="time-block" id="'+ 
-        index + 
-        '"><div class="row no-gutters"><div class="col-1">'+
-        timeLabel + 
-        '</div><textarea class="form-control col-10">' +
-        timeBlock.event + 
-        '</textarea><div class="col-1"><button class="saveBtn btn-block"type="submit"><i class="bi bi-save"></i></button></div></div></div>';
-    $(".container").append(row);
+
+// get loadedTasks
+var getTasks = function(){
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks = loadedTasks;
+
+    console.log(loadedTasks)
+    $.each(tasks, function(hour, task){
+        var hourDiv = $("#" + hour);
+        createTask(task, hourDiv);
+    });
+}
+
+// save tasks 
+$(".saveBtn").click(function(){
+    var taskTime = $(this).siblings(".hour").text();
+    console.log(taskTime);
+    var taskText = $(this).siblings(".task").val();
+    console.log(taskText);
+    
+    tasks[taskTime] = taskText;
+  
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 });
 
-// save work hour descriptions 
-let saveTasks = function(){
-    localStorage.setItem("workTasks",JSON.stringify(workHours));
-}
-saveTasks();
+getTasks();
